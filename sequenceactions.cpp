cb70afe11060206bb00f66f617702902fbfd15e0
snowkurtis83-dot <snowkurtis83@gmail.com>
2026-02-18 14:24:53 +0100

    Update sequenceactions.cpp

--- sequenceactions.cpp
+++ sequenceactions.cpp
@@ -1,23 +1,13 @@
-#include <thread>
-#include <chrono>
-#include "iostream"
-using namespace std;
-
 #include "sequenceactions.h"
-
 
 void SequenceActions::ajouter(ActionRobot* p_action) {
     m_actions.push_back(p_action);
 }
 
 void SequenceActions::executer(ContexteRobot& ctx) {
-    cout<<"---Execution du plan ---"<<endl;
     for (std::size_t i = 0; i < m_actions.size(); ++i) {
-        cout<<"Action : ";
-        m_actions[i]->executer(ctx);
         m_actions[i]->afficherNom();
-        cout<<"Action : Attendre 200ms"<<endl;
-        this_thread::sleep_for(chrono::seconds(2));
+        m_actions[i]->executer(ctx);
     }
 }
 
